5222869e2e1716f29a78e50883a05f387c9c2150
Nicholas Bishop <nicholasbishop@gmail.com>
2015-04-24 19:16:44 -0400

    Iter test passing

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,20 @@
-extern crate num;
-
-use std::ops::Add;
-use num::Unsigned;
-
 /// 2D grid coordinate
-#[derive(Clone, Copy, Eq, Debug, PartialEq)]
-pub struct Coord<T: Unsigned> {
-    pub x: T,
-    pub y: T
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Coord {
+    pub x: usize,
+    pub y: usize
 }
 
-impl<T: Unsigned> Coord<T> {
+/// 2D grid dimensions
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct Size {
+    pub width: usize,
+    pub height: usize
+}
+
+impl Coord {
     /// Create a grid coordinate at (x, y)
-    pub fn new(x: T, y: T) -> Coord<T> {
+    pub fn new(x: usize, y: usize) -> Coord {
         Coord {
             x: x,
             y: y
@@ -20,21 +22,123 @@ impl<T: Unsigned> Coord<T> {
     }
 }
 
-/// Rectangle defined by inclusive minimum and maximum coordinates
-#[derive(Clone, Copy, Eq, Debug, PartialEq)]
-pub struct Rect<T: Copy + Unsigned> {
-    /// Minimum coordinate (inclusive)
-    pub min_coord: Coord<T>,
+impl Size {
+    /// Create a grid size of (width, height)
+    pub fn new(width: usize, height: usize) -> Size {
+        Size {
+            width: width,
+            height: height
+        }
+    }
 
-    /// Maximum coordinate (inclusive)
-    pub max_coord: Coord<T>
+    /// Return true if the coordinate fits within self's width and
+    /// height, false otherwise.
+    pub fn contains_coord(&self, coord: Coord) -> bool {
+        coord.x < self.width && coord.y < self.height
+    }
 }
 
-impl<T: Copy + PartialOrd + Unsigned> Rect<T> {
+/// Rectangle defined by inclusive minimum and maximum coordinates
+#[derive(Clone, Copy, Eq, Debug, PartialEq)]
+pub struct Rect {
+    /// Minimum coordinate (inclusive)
+    min_coord: Coord,
+
+    /// Maximum coordinate (inclusive)
+    max_coord: Coord
+}
+
+impl Rect {
+    fn width(&self) -> usize {
+        return self.max_coord.x - self.min_coord.x + 1;
+    }
+}
+
+pub struct GridMut<'a, Elem: 'a> {
+    elems: &'a mut [Elem],
+    size: Size
+}
+
+impl<'a, Elem> GridMut<'a, Elem> {
+    pub fn new(elems: &'a mut [Elem], size: Size) -> Option<GridMut<Elem>> {
+        if size.width * size.height == elems.len() {
+            Some(GridMut {
+                elems: elems,
+                size: size
+            })
+        }
+        else {
+            None
+        }
+    }
+
+    pub fn rect_iter_mut(&'a mut self, rect: Rect) -> Option<RectIterMut<'a, Elem>> {
+        if self.size.contains_coord(rect.max_coord) {
+            Some(RectIterMut {
+                stride: (self.size.width - rect.width()) as isize,
+                cur_elem: self.elems.as_mut_ptr(),
+                grid: self,
+                rect: rect,
+                cur_coord: rect.min_coord
+            })
+        }
+        else {
+            None
+        }
+    }
+}
+
+impl<'a, Elem> Iterator for RectIterMut<'a, Elem> {
+    type Item = (Coord, &'a mut Elem);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur_coord.y <= self.rect.max_coord.y {
+            let result = (self.cur_coord, unsafe { &mut *self.cur_elem });
+
+            self.cur_coord.x += 1;
+            if self.cur_coord.x <= self.rect.max_coord.x {
+                unsafe { self.cur_elem.offset(1); }
+            }
+            else {
+                self.cur_coord.x = self.rect.min_coord.x;
+                self.cur_coord.y += 1;
+                unsafe { self.cur_elem.offset(self.stride); }
+            }
+            Some(result)
+        }
+        else {
+            None
+        }
+    }
+}
+
+pub struct RectIterMut<'a, Elem: 'a> {
+    grid: &'a mut GridMut<'a, Elem>,
+    rect: Rect,
+    cur_elem: *mut Elem,
+    cur_coord: Coord,
+    stride: isize
+}
+
+#[test]
+fn test_rect_iter_mut() {
+    let mut elems = [0, 1, 2, 3];
+    let mut grid = GridMut::new(&mut elems, Size::new(2, 2)).unwrap();
+    let rect = Rect::new(Coord::new(0, 0), Coord::new(1, 1)).unwrap();
+
+    let mut actual_coords = Vec::new();
+    for (coord, elem) in grid.rect_iter_mut(rect).unwrap() {
+        *elem = -(*elem);
+        actual_coords.push((coord.x, coord.y));
+    }
+    assert_eq!(actual_coords, [(0, 0), (1, 0), (0, 1), (1, 1)]);
+}
+
+impl Rect {
     /// Create a new Rect defined by inclusive minimum and maximum
     /// coordinates. If min_coord is greater than max_coord on either
     /// axis then None is returned.
-    pub fn new(min_coord: Coord<T>, max_coord: Coord<T>) -> Option<Rect<T>> {
+    pub fn new(min_coord: Coord, max_coord: Coord) -> Option<Rect> {
         if min_coord.x <= max_coord.x && min_coord.y <= max_coord.y {
             Some(Rect {
                 min_coord: min_coord,
@@ -46,76 +150,58 @@ impl<T: Copy + PartialOrd + Unsigned> Rect<T> {
         }
     }
 
-    /// Iterate from minimum coord to maximum coord by row.
-    pub fn iter(&self) -> RectIter<T> {
-        RectIter {
-            rect: *self,
-            cur_coord: self.min_coord
-        }
-    }
-
-    pub fn subrect_iter<'s, S>(&self, subrect: Rect<T>, data: &'s [S])
-                               -> DataRectIter<'s, S, T> {
-        // TODO(nicholasbishop): validate subrect
-        DataRectIter {
-            data: data,
-            cur_elem: data.as_ptr(),
-            cur_coord: subrect.min_coord,
-            full: *self,
-            part: subrect
-        }
-    }
 }
 
-pub struct RectIter<T: Copy + Unsigned> {
-    rect: Rect<T>,
-    cur_coord: Coord<T>
-}
+// pub struct RectIter<usize: Copy + Unsigned> {
+//     rect: Rect,
+//     cur_coord: Coord
+// }
 
-impl<T: Copy + Ord + Unsigned + Add<Output=T> + num::One> Iterator for RectIter<T> {
-    type Item = Coord<T>;
+// impl<usize: Copy + Ord + Unsigned + Add<Output=usize> + num::One> Iterator for RectIter {
+//     type Item = Coord;
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.cur_coord.y <= self.rect.max_coord.y {
-            let result = Some(self.cur_coord);
-            self.cur_coord.x = self.cur_coord.x + T::one();
-            if self.cur_coord.x > self.rect.max_coord.x {
-                self.cur_coord.x = self.rect.min_coord.x;
-                self.cur_coord.y = self.cur_coord.y + T::one();
-            }
-            result
-        }
-        else {
-            None
-        }
-    }
-}
+//     fn next(&mut self) -> Option<Self::Item> {
+//         if self.cur_coord.y <= self.rect.max_coord.y {
+//             let result = Some(self.cur_coord);
+//             self.cur_coord.x = self.cur_coord.x + usize::one();
+//             if self.cur_coord.x > self.rect.max_coord.x {
+//                 self.cur_coord.x = self.rect.min_coord.x;
+//                 self.cur_coord.y = self.cur_coord.y + usize::one();
+//             }
+//             result
+//         }
+//         else {
+//             None
+//         }
+//     }
+// }
 
-#[test]
-fn test_rect_iter() {
-    let rect = Rect::new(Coord::new(1, 2), Coord::new(3, 4)).unwrap();
-    let coords: Vec<Coord<u8>> = rect.iter().collect();
-    assert_eq!(coords, [
-        Coord::new(1, 2), Coord::new(2, 2), Coord::new(3, 2),
-        Coord::new(1, 3), Coord::new(2, 3), Coord::new(3, 3),
-        Coord::new(1, 4), Coord::new(2, 4), Coord::new(3, 4)]);
-}
+// #[test]
+// fn test_rect_iter() {
+//     let rect = Rect::new(Coord::new(1, 2), Coord::new(3, 4)).unwrap();
+//     let coords: Vec<Coord<u8>> = rect.iter().collect();
+//     assert_eq!(coords, [
+//         Coord::new(1, 2), Coord::new(2, 2), Coord::new(3, 2),
+//         Coord::new(1, 3), Coord::new(2, 3), Coord::new(3, 3),
+//         Coord::new(1, 4), Coord::new(2, 4), Coord::new(3, 4)]);
+// }
 
-pub struct DataRectIter<'s, S: 's, T: Copy + Unsigned> {
-    data: &'s [S],
-    cur_elem: *const S,
-    cur_coord: Coord<T>,
-    full: Rect<T>,
-    part: Rect<T>
-}
+// pub struct DataRectIter<'s, S: 's, usize: Copy + Unsigned> {
+//     data: &'s [S],
+//     cur_elem: *const S,
+//     cur_coord: Coord,
+//     full: Rect,
+//     part: Rect
+// }
 
-impl<'s, S: 's, T: Copy + Unsigned> Iterator for DataRectIter<'s, S, T> {
-    type Item = (Coord<T>, &'s S);
+// impl<'s, S: 's, usize: Copy + Unsigned> Iterator for DataRectIter<'s, S, usize> {
+//     type Item = (Coord, &'s S);
 
-    fn next(&mut self) -> Option<Self::Item> {
-        unsafe {
-            self.cur_elem = self.cur_elem.offset(1);
-        }
-        None
-    }
-}
+//     fn next(&mut self) -> Option<Self::Item> {
+//         unsafe {
+//             self.cur_elem = self.cur_elem.offset(1);
+//         }
+//         None
+//     }
+// }
+
