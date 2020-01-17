05dfffdb8e7538b847f2ba55337a1dae125e10c5
Leon Matthes <leon@matthes.biz>
2020-01-16 19:37:51 -0500

    Use clone trait instead of copy and make it optional

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ pub struct Coord {
     pub x: usize,
 
     /// Y component
-    pub y: usize
+    pub y: usize,
 }
 
 /// Rectangle defined by inclusive minimum and maximum coordinates
@@ -33,7 +33,7 @@ pub struct Rect {
     min_coord: Coord,
 
     /// Maximum coordinate (inclusive)
-    max_coord: Coord
+    max_coord: Coord,
 }
 
 /// Rectangle dimensions
@@ -42,14 +42,14 @@ pub struct Size {
     /// Width of rectangle
     pub width: usize,
     /// Height of rectangle
-    pub height: usize
+    pub height: usize,
 }
 
 /// Container for 2D data
 #[derive(Clone, Debug)]
 pub struct Vec2D<T> {
     elems: Vec<T>,
-    size: Size
+    size: Size,
 }
 
 /// Iterator over a rectangle within a Vec2D
@@ -59,7 +59,7 @@ pub struct RectIter<'a, Elem: 'a> {
     rect: Rect,
     cur_elem: *const Elem,
     cur_coord: Coord,
-    stride: isize
+    stride: isize,
 }
 
 /// Mutable iterator over a rectangle within a Vec2D
@@ -69,16 +69,13 @@ pub struct RectIterMut<'a, Elem: 'a> {
     rect: Rect,
     cur_elem: *mut Elem,
     cur_coord: Coord,
-    stride: isize
+    stride: isize,
 }
 
 impl Coord {
     /// Create a coordinate at (x, y)
     pub fn new(x: usize, y: usize) -> Coord {
-        Coord {
-            x: x,
-            y: y
-        }
+        Coord { x: x, y: y }
     }
 }
 
@@ -86,8 +83,7 @@ impl std::ops::Add for Coord {
     type Output = Coord;
 
     fn add(self, other: Coord) -> Coord {
-        Coord::new(self.x + other.x,
-                   self.y + other.y)
+        Coord::new(self.x + other.x, self.y + other.y)
     }
 }
 
@@ -110,8 +106,10 @@ impl Rect {
     /// Return true if the coordinate is between `min_coord` and
     /// `max_coord` (inclusive).
     pub fn contains_coord(&self, coord: Coord) -> bool {
-        (coord.x >= self.min_coord.x && coord.x <= self.max_coord.x &&
-         coord.y >= self.min_coord.y && coord.y <= self.max_coord.y)
+        (coord.x >= self.min_coord.x
+            && coord.x <= self.max_coord.x
+            && coord.y >= self.min_coord.y
+            && coord.y <= self.max_coord.y)
     }
 }
 
@@ -120,7 +118,7 @@ impl Size {
     pub fn new(width: usize, height: usize) -> Size {
         Size {
             width: width,
-            height: height
+            height: height,
         }
     }
 
@@ -139,21 +137,37 @@ impl Size {
     pub fn rect(&self) -> Rect {
         Rect {
             min_coord: Coord::new(0, 0),
-            max_coord: Coord::new(self.width - 1, self.height - 1)
+            max_coord: Coord::new(self.width - 1, self.height - 1),
         }
     }
 }
 
-impl<Elem: Copy> Vec2D<Elem> {
+impl<Elem: Clone> Vec2D<Elem> {
     /// Create a Vec2D with the given `size`. All elements are
     /// initialized as copies of the `example` element.
+    ///
+    /// ```
+    /// # use vec2d::{Vec2D, Size};
+    /// let vector = Vec2D::from_example(Size::new(10, 10), &42);
+    /// for (_coord, &item) in vector.iter() {
+    ///     assert_eq!(item, 42);
+    /// }
+    /// ```
     pub fn from_example(size: Size, example: &Elem) -> Vec2D<Elem> {
         Vec2D {
-            elems: vec![*example; size.area()],
-            size: size
+            elems: vec![example.clone(); size.area()],
+            size: size,
         }
     }
 
+    /// Resize in-place so that `size()` is equal to `new_size`
+    pub fn resize(&mut self, new_size: Size, value: Elem) {
+        self.elems.resize(new_size.area(), value);
+        self.size = new_size;
+    }
+}
+
+impl<Elem> Vec2D<Elem> {
     /// Create a Vec2D with the given `size`. The contents are set to
     /// `src`. None is returned if the `size` does not match the
     /// length of `src`.
@@ -161,15 +175,14 @@ impl<Elem: Copy> Vec2D<Elem> {
         if size.area() == src.len() {
             Some(Vec2D {
                 elems: src,
-                size: size
+                size: size,
             })
-        }
-        else {
+        } else {
             None
         }
     }
 
-    /// Returns element at the given coord or `None` if the coord is 
+    /// Returns element at the given coord or `None` if the coord is
     /// outside the Vec2D
     ///
     /// # Example
@@ -187,10 +200,10 @@ impl<Elem: Copy> Vec2D<Elem> {
     /// # }
     /// ```
     pub fn get(&self, coord: Coord) -> Option<&Elem> {
-        if self.size.contains_coord (coord) {
+        if self.size.contains_coord(coord) {
             // column major coords
-            let i = coord.y*self.size.width + coord.x;
-            return Some(&self.elems[i])
+            let i = coord.y * self.size.width + coord.x;
+            return Some(&self.elems[i]);
         }
         None
     }
@@ -213,10 +226,10 @@ impl<Elem: Copy> Vec2D<Elem> {
     /// # }
     /// ```
     pub fn get_mut(&mut self, coord: Coord) -> Option<&mut Elem> {
-        if self.size.contains_coord (coord) {
+        if self.size.contains_coord(coord) {
             // column major coords
-            let i = coord.y*self.size.width + coord.x;
-            return Some(&mut self.elems[i])
+            let i = coord.y * self.size.width + coord.x;
+            return Some(&mut self.elems[i]);
         }
         None
     }
@@ -224,12 +237,6 @@ impl<Elem: Copy> Vec2D<Elem> {
     /// Shortcut for self.size.rect()
     pub fn rect(&self) -> Rect {
         self.size.rect()
-    }
-
-    /// Resize in-place so that `size()` is equal to `new_size`
-    pub fn resize(&mut self, new_size: Size, value: Elem) {
-        self.elems.resize (new_size.area(), value);
-        self.size = new_size;
     }
 
     /// Width and height
@@ -263,20 +270,16 @@ impl<Elem: Copy> Vec2D<Elem> {
     /// the `start` coord. None is returned if the given `rect` does
     /// not fit entirely within the Vec2D or if the `start` coord is
     /// not within `rect`.
-    pub fn rect_iter_at<'a>(&'a self, rect: Rect,
-                            start: Coord) -> Option<RectIter<'a, Elem>> {
+    pub fn rect_iter_at<'a>(&'a self, rect: Rect, start: Coord) -> Option<RectIter<'a, Elem>> {
         if self.size.contains_coord(rect.max_coord) && rect.contains_coord(start) {
             Some(RectIter {
                 grid: std::marker::PhantomData,
                 stride: self.stride(&rect),
-                cur_elem: unsafe {
-                    self.elems.as_ptr().offset(self.start_offset(start))
-                },
+                cur_elem: unsafe { self.elems.as_ptr().offset(self.start_offset(start)) },
                 rect: rect,
-                cur_coord: start
+                cur_coord: start,
             })
-        }
-        else {
+        } else {
             None
         }
     }
@@ -298,20 +301,20 @@ impl<Elem: Copy> Vec2D<Elem> {
     /// Vec2D with the `start` coord. None is returned if the given
     /// `rect` does not fit entirely within the Vec2D or if the
     /// `start` coord is not within `rect`.
-    pub fn rect_iter_mut_at<'a>(&'a mut self, rect: Rect,
-                            start: Coord) -> Option<RectIterMut<'a, Elem>> {
+    pub fn rect_iter_mut_at<'a>(
+        &'a mut self,
+        rect: Rect,
+        start: Coord,
+    ) -> Option<RectIterMut<'a, Elem>> {
         if self.size.contains_coord(rect.max_coord) && rect.contains_coord(start) {
             Some(RectIterMut {
                 grid: std::marker::PhantomData,
                 stride: self.stride(&rect),
-                cur_elem: unsafe {
-                    self.elems.as_mut_ptr().offset(self.start_offset(start))
-                },
+                cur_elem: unsafe { self.elems.as_mut_ptr().offset(self.start_offset(start)) },
                 rect: rect,
-                cur_coord: start
+                cur_coord: start,
             })
-        }
-        else {
+        } else {
             None
         }
     }
@@ -322,20 +325,22 @@ impl<'a, Elem> Iterator for RectIter<'a, Elem> {
 
     fn next(&mut self) -> Option<Self::Item> {
         if self.cur_coord.y <= self.rect.max_coord.y {
-            let result = (self.cur_coord, unsafe { & *self.cur_elem });
+            let result = (self.cur_coord, unsafe { &*self.cur_elem });
 
             self.cur_coord.x += 1;
             if self.cur_coord.x <= self.rect.max_coord.x {
-                unsafe { self.cur_elem = self.cur_elem.offset(1); }
-            }
-            else {
+                unsafe {
+                    self.cur_elem = self.cur_elem.offset(1);
+                }
+            } else {
                 self.cur_coord.x = self.rect.min_coord.x;
                 self.cur_coord.y += 1;
-                unsafe { self.cur_elem = self.cur_elem.offset(self.stride); }
+                unsafe {
+                    self.cur_elem = self.cur_elem.offset(self.stride);
+                }
             }
             Some(result)
-        }
-        else {
+        } else {
             None
         }
     }
@@ -350,16 +355,18 @@ impl<'a, Elem> Iterator for RectIterMut<'a, Elem> {
 
             self.cur_coord.x += 1;
             if self.cur_coord.x <= self.rect.max_coord.x {
-                unsafe { self.cur_elem = self.cur_elem.offset(1); }
-            }
-            else {
+                unsafe {
+                    self.cur_elem = self.cur_elem.offset(1);
+                }
+            } else {
                 self.cur_coord.x = self.rect.min_coord.x;
                 self.cur_coord.y += 1;
-                unsafe { self.cur_elem = self.cur_elem.offset(self.stride); }
+                unsafe {
+                    self.cur_elem = self.cur_elem.offset(self.stride);
+                }
             }
             Some(result)
-        }
-        else {
+        } else {
             None
         }
     }
@@ -373,14 +380,12 @@ impl Rect {
         if min_coord.x <= max_coord.x && min_coord.y <= max_coord.y {
             Some(Rect {
                 min_coord: min_coord,
-                max_coord: max_coord
+                max_coord: max_coord,
             })
-        }
-        else {
+        } else {
             None
         }
     }
-
 }
 
 #[cfg(test)]
@@ -416,10 +421,14 @@ mod test {
 
     #[test]
     fn test_bad_rect() {
-        assert_eq!(Rect::new(Coord::new(2, 1), Coord::new(1, 1)).is_none(),
-                   true);
-        assert_eq!(Rect::new(Coord::new(1, 2), Coord::new(1, 1)).is_none(),
-                   true);
+        assert_eq!(
+            Rect::new(Coord::new(2, 1), Coord::new(1, 1)).is_none(),
+            true
+        );
+        assert_eq!(
+            Rect::new(Coord::new(1, 2), Coord::new(1, 1)).is_none(),
+            true
+        );
     }
 
     #[test]
@@ -461,7 +470,7 @@ mod test {
 
         let iter1 = v.rect_iter(size.rect()).unwrap();
         let iter2 = v.rect_iter(size.rect()).unwrap();
-        
+
         for ((coord1, elem1), (coord2, elem2)) in iter1.zip(iter2) {
             assert_eq!(coord1, coord2);
             assert_eq!(elem1, elem2);
